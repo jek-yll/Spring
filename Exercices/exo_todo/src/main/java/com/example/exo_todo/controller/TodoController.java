01a567cbf6f1eaa54bb37cb4405ee580d8254e50
loic <loic.gstrn@gmail.com>
2024-02-12 15:27:45 +0100

    exo-todo

--- Exercices/exo_todo/src/main/java/com/example/exo_todo/controller/TodoController.java
+++ Exercices/exo_todo/src/main/java/com/example/exo_todo/controller/TodoController.java
@@ -1,12 +1,18 @@
 package com.example.exo_todo.controller;
 
-import com.example.exo_todo.model.Todo;
+import com.example.exo_todo.service.TodoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TodoController {
+
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
 
     @RequestMapping("/")
     public String home(){
@@ -15,8 +21,7 @@ public class TodoController {
 
     @RequestMapping("/todo1")
     public String todoDetails(Model model){
-        Todo todo = Todo.builder().name("Faire une todolist").description("Description de la tache : Faire une todolist").done(false).build();
-        model.addAttribute("todo", todo);
+        model.addAttribute("todo", todoService.todosList().get(1));
         return "todo/todo-details";
     }
 }
--- Exercices/exo_todo/src/main/java/com/example/exo_todo/controller/TodoRestController.java
+++ Exercices/exo_todo/src/main/java/com/example/exo_todo/controller/TodoRestController.java
@@ -1,34 +1,31 @@
 package com.example.exo_todo.controller;
 
 import com.example.exo_todo.model.Todo;
+import com.example.exo_todo.service.TodoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/base")
 public class TodoRestController {
 
+    private final TodoService todoService;
+
+    public TodoRestController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
     @GetMapping("/todo")
     public Todo todo(){
-        Todo todo = Todo.builder().name("Creer une todolist").description("Description de la tache : Creer une todolist").done(false).build();
-
-        return todo;
+        return todoService.todosList().get(0);
     }
 
     @GetMapping("/todos")
     public List<Todo> todos(){
-        List <Todo> todos = new ArrayList<>();
-
-        todos.add(Todo.builder().name("Creer une todolist").description("Description de la tache : Creer une todolist").done(false).build());
-        todos.add(Todo.builder().name("Mettre a jour une todolist").description("Description de la tache : Mettre a jour une todolist").done(true).build());
-        todos.add(Todo.builder().name("Supprimer une todolist").description("Description de la tache : Supprimer une todolist").done(true).build());
-        todos.add(Todo.builder().name("Lire une todolist").description("Description de la tache : Lire une todolist").done(false).build());
-
-        return todos;
+        return todoService.todosList();
     }
 
 }
