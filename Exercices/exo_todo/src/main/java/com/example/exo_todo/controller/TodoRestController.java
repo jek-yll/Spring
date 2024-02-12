231e3af62851c81bfc235054550e4881cda86c43
loic <loic.gstrn@gmail.com>
2024-02-12 16:17:10 +0100

    exo-todo

--- Exercices/exo_todo/src/main/java/com/example/exo_todo/controller/TodoRestController.java
+++ Exercices/exo_todo/src/main/java/com/example/exo_todo/controller/TodoRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/base")
+@RequestMapping("/api/v1/base")
 public class TodoRestController {
 
     private final TodoService todoService;
