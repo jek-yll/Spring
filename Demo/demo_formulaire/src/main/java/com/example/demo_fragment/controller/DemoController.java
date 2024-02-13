5bd5c41c5701697ab7d74d86d5d3cc3027ce581d
loic <loic.gstrn@gmail.com>
2024-02-13 14:00:27 +0100

    add demo formulaire

---
+++ Demo/demo_formulaire/src/main/java/com/example/demo_fragment/controller/DemoController.java
@@ -0,0 +1,40 @@
+package com.example.demo_fragment.controller;
+
+import com.example.demo_fragment.model.Rabbit;
+import com.example.demo_fragment.service.RabbitService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequiredArgsConstructor
+public class DemoController {
+
+    private final RabbitService rabbitService;
+
+    @GetMapping("/")
+    public String homePage(){
+        return "pagea";
+    }
+
+    @GetMapping("/pageb")
+    public String pageb(Model model) {
+        List<Rabbit> rabbits = rabbitService.getRabbits();
+        Rabbit rabbit = rabbits.get(0);
+        model.addAttribute("rabbitId", rabbit.getId());
+        model.addAttribute("rabbits", rabbits);
+        return "pageb";
+    }
+
+    @GetMapping("/detail/{rabbitId}")
+    public String detailRabbit(@PathVariable("rabbitId")UUID id, Model model) {
+        Rabbit rabbit = rabbitService.getRabbitById(id);
+        model.addAttribute("myRabbit", rabbit);
+        return "pagec";
+    }
+}
