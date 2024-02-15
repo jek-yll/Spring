cc5c103df2417bafd1ccd21a50c3b802ea791596
loic <loic.gstrn@gmail.com>
2024-02-15 12:08:07 +0100

    exo student v2

---
+++ Demo/demo_validation/src/main/java/com/example/demo_validation/controller/DemoController.java
@@ -0,0 +1,27 @@
+package com.example.demo_validation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "/hello";
+    }
+
+//    @GetMapping("/pb")
+//    public String pb(){
+//        if (true){
+//        throw new RuntimeException("Problème");
+//        }
+//        return "hello";
+//    }
+
+}
