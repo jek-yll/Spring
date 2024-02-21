aeab1bd9867ab10ad4765c8a8410e26d1a2727d8
loic <loic.gstrn@gmail.com>
2024-02-21 09:13:56 +0100

    add user suite

--- Exercices/tp_blog/src/main/java/org/example/tp_blog/dao/IUserRepository.java
+++ Exercices/tp_blog/src/main/java/org/example/tp_blog/dao/IUserRepository.java
@@ -2,11 +2,9 @@ package org.example.tp_blog.dao;
 
 import org.example.tp_blog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-@Repository
 public interface IUserRepository extends JpaRepository<User, UUID> {
 
     User findByIdIs(UUID id);
