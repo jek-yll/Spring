c25a49765d1b3ede583f772cd8d184538d66a9d8
loic <loic.gstrn@gmail.com>
2024-02-21 09:11:16 +0100

    add user suite

--- Exercices/tp_blog/src/main/java/org/example/tp_blog/dao/ICommentRepository.java
+++ Exercices/tp_blog/src/main/java/org/example/tp_blog/dao/ICommentRepository.java
@@ -3,6 +3,7 @@ package org.example.tp_blog.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.example.tp_blog.model.Comment;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
--- Exercices/tp_blog/src/main/java/org/example/tp_blog/dao/IPostRepository.java
+++ Exercices/tp_blog/src/main/java/org/example/tp_blog/dao/IPostRepository.java
@@ -2,6 +2,7 @@ package org.example.tp_blog.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.example.tp_blog.model.Post;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
